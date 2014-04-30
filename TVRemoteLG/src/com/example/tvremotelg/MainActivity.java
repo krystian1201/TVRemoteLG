509ca91e38fcd1c72a4a5d49363a74ee3bd6deaa
krystian1201 <krystian1201@gmail.com>
2014-04-30 19:45:52 +0900

    Added labels for channel and volume. Power button is also black.

--- TVRemoteLG/src/com/example/tvremotelg/MainActivity.java
+++ TVRemoteLG/src/com/example/tvremotelg/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 
